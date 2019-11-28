bab2e651ce33b9bab33a0b05af71ff53391dab77
azd1997 <374192922@qq.com>
2019-11-28 07:26:59 -0500

    20191128-zinx-connection-3

--- src/net/connection.go
+++ src/net/connection.go
@@ -1,6 +1,7 @@
 package net
 
 import (
+	"fmt"
 	"github.com/azd1997/zinx/src/iface"
 	"net"
 )
@@ -37,7 +38,10 @@ func NewConnection(conn *net.TCPConn, id uint32, callbackAPI iface.HandleFunc) *
 
 // Start 启动连接
 func (c *Connection) Start() {
+	fmt.Printf("Conn start... conn ID = %d\n", c.ID)
 
+	// 启动当前连接的读数据业务
+	go c.startReader()
 }
 
 // Stop 停止连接
@@ -47,20 +51,51 @@ func (c *Connection) Stop() {
 
 // GetTCPConn 获取当前连接绑定的socket
 func (c *Connection) GetTCPConn() *net.TCPConn {
-
+	return c.Conn
 }
 
 // GetConnID 获取当前连接的ID
 func (c *Connection) GetConnID() uint32 {
-
+	return c.ID
 }
 
 // RemoteAddr 获取远程客户端的TCP状态 IP Port
 func (c *Connection) RemoteAddr() net.Addr {
-
+	return c.Conn.RemoteAddr()
 }
 
 // Send 发送数据
 func (c *Connection) Send(data []byte) error {
 
+	// TODO: 待实现，这里随便写的
+
+	cnt, err := c.Conn.Write(data)
+	if cnt != len(data) || err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// startReader 启动连接的读数据，持续从客户端读取数据，并交给handleAPI处理
+func (c *Connection) startReader() {
+	fmt.Println("Reader goroutine is running...")
+	defer fmt.Printf("connID = %d; Reader exits, remote addr is %s\n", c.ID, c.RemoteAddr())
+	defer c.Stop()
+
+	for {
+		// 读客户端数据到buffer区
+		buf := make([]byte, 512)
+		cnt, err := c.Conn.Read(buf)
+		if err != nil {
+			fmt.Printf("Receive buf error: %s\n", err)
+			continue
+		}
+
+		// 调用当前连接所绑定的handleAPI去处理客户端传来的数据
+		if err = c.handleAPI(c.Conn, buf, cnt); err != nil {
+			fmt.Printf("Conn #%d: handleAPI error: %s\n", c.ID, err)
+			break
+		}
+	}
 }
